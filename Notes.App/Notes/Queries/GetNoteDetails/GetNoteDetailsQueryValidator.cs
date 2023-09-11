dc0aaf6e7900e759f44f6e89b67aa912ff827277
Amir Bazanov <flash5651311@gmail.com>
2023-09-11 21:44:22 +0500

    Added GetNoteDetailsQuery, Handler and Validator

---
+++ Notes.App/Notes/Queries/GetNoteDetails/GetNoteDetailsQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Notes.App.Notes.Queries.GetNoteDetails; 
+
+public class GetNoteDetailsQueryValidator : AbstractValidator<GetNoteDetailsQuery>
+{
+    public GetNoteDetailsQueryValidator()
+    {
+        RuleFor(note => note.Id).NotEqual(Guid.Empty);
+        RuleFor(note => note.UserId).NotEqual(Guid.Empty);
+    }
+}
